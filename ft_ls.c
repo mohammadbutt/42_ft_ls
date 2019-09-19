0fa9d644496384f132bc12527f6807ff4b6d1356
Mohammad Butt <mbutt@student.42.us.org>
2019-09-18 22:21:44 -0700

    Created a simple ls

--- ft_ls.c
+++ ft_ls.c
@@ -6,14 +6,38 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/18 21:19:28 by mbutt             #+#    #+#             */
-/*   Updated: 2019/09/18 21:23:34 by mbutt            ###   ########.fr       */
+/*   Updated: 2019/09/18 22:21:03 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
+int main(int argc, char **argv)
+{
+	struct dirent *pDirent;
+	DIR *pDir;
+
+	if(argc < 2)
+	{
+		ft_printf("Useage: testprog <dirname>\n");
+		return(1);
+	}
+	pDir = opendir(argv[1]);
+	if(pDir == NULL)
+	{
+		ft_printf("Cannot open directory '%s'\n", argv[1]);
+		return(1);
+	}
+	while((pDirent = readdir(pDir)) != NULL)
+		ft_printf("[%s]\n", pDirent->d_name);
+	closedir(pDir);
+	return(0);
+}
+
+/*
 int main(void)
 {
 	   printf("   printf:|This is a test.|\n");
-	ft_printf("ft_printf:|This is a test.|\n")
+	ft_printf("ft_printf:|This is a test.|\n");
 }
+*/
--- ft_ls.h
+++ ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/18 21:19:42 by mbutt             #+#    #+#             */
-/*   Updated: 2019/09/18 21:23:15 by mbutt            ###   ########.fr       */
+/*   Updated: 2019/09/18 21:48:47 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef FT_LS_H
@@ -14,6 +14,7 @@
 
 #include "ft_printf/srcs/ft_printf.h"
 #include "ft_printf/srcs/mini_libft/mini_libft.h"
+#include <dirent.h>
 #include <stdio.h>
 
 #endif
