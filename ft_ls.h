f41a4267b3e517d346eccfff2f55bd9a43c8c442
Mohammad Butt <mbutt@student.42.us.org>
2019-09-18 21:24:00 -0700

    Created header file

--- ft_ls.h
+++ ft_ls.h
@@ -1,21 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/09/18 16:18:32 by mbutt             #+#    #+#             */
-/*   Updated: 2019/09/18 20:07:12 by mbutt            ###   ########.fr       */
+/*   Created: 2019/09/18 21:19:42 by mbutt             #+#    #+#             */
+/*   Updated: 2019/09/18 21:23:15 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#ifndef FT_LS_H
+# define FT_LS_H
 
 #include "ft_printf/srcs/ft_printf.h"
 #include "ft_printf/srcs/mini_libft/mini_libft.h"
 #include <stdio.h>
 
-int main(void)
-{
-	ft_printf("This is a test\n");
-	printf("This is also a test\n");
-}
+#endif
