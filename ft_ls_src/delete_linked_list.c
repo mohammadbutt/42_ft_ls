dae584efed5f6fc26cbd2f1d0c3a4c2e4b7993f9
Mohammad Butt <maniqrb@gmail.com>
2019-12-19 15:58:19 -0800

    Update delete_linked_list.c

--- ft_ls_src/delete_linked_list.c
+++ ft_ls_src/delete_linked_list.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include "ft_ls.h"
-
+/*
 void	delete_list_file_name(t_ls **head_ref)
 {
 	t_ls *current_node;
@@ -27,5 +27,25 @@ void	delete_list_file_name(t_ls **head_ref)
 			free(current_node);
 		current_node = next_node;
 	}
+	*head_ref = NULL;
+}
+*/
+void	delete_list_file_name(t_ls **head_ref)
+{
+	t_ls *current_node;
+//	t_ls *next_node;
+
+	current_node = *head_ref;
+	while (current_node != NULL)
+	{
+//		next_node = current_node->next;
+		if (current_node->file_name)
+			free(current_node->file_name);
+		if (current_node)
+			free(current_node);
+//		current_node = next_node;
+		current_node = current_node->next;
+	}
+	free(*head_ref);
 	*head_ref = NULL;
 }
